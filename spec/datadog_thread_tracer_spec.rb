972227dd4dfef5d7e5c1abd4a51f5ffed92c2587
sue445 <sue445@sue445.net>
2022-07-07 21:52:09 +0900

    Remobe dummy test

--- spec/datadog_thread_tracer_spec.rb
+++ spec/datadog_thread_tracer_spec.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe DatadogThreadTracer do
+  # TODO: Remove after
   it "has a version number" do
     expect(DatadogThreadTracer::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
