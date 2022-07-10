3c204d3e8ef548cc28e317c952b1cc5f919b07c9
sue445 <sue445@sue445.net>
2022-07-10 12:51:34 +0900

    Support thread args

--- lib/datadog_thread_tracer/thread_tracer.rb
+++ lib/datadog_thread_tracer/thread_tracer.rb
@@ -9,19 +9,22 @@ module DatadogThreadTracer
     end
 
     # @param trace_name [String]
+    # @param thread_args [Array,Object] Passed to `Thread.start`
     # @yield Processes you want to execute in a thread
-    def trace(trace_name = nil, &block)
+    def trace(trace_name: nil, thread_args: [])
       @thread_count += 1
       trace_name ||= "thread_#{@thread_count}"
 
       tracer = Datadog::Tracing.send(:tracer)
 
-      @threads << Thread.start(trace_name, tracer.provider.context) do |name, context|
+      @threads << Thread.start(trace_name, tracer.provider.context, Array(thread_args)) do |name, context, args|
         tracer = Datadog::Tracing.send(:tracer)
 
         # c.f. https://github.com/DataDog/dd-trace-rb/issues/1460
         tracer.provider.context = context
-        Datadog::Tracing.trace(name, &block)
+        Datadog::Tracing.trace(name) do
+          yield(*args)
+        end
       end
     end
 
